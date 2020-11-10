64a4295effaa8af10092f507daed94f7b1eab901
Takashi Imagire <imagire@gmail.com>
2020-11-10 19:14:56 +0900

    Add files via upload

---
+++ src/StaticLibTree/tree.c
@@ -0,0 +1,38 @@
+﻿#include "Windows.h"                    // Windows API の機能定義
+#include <stdbool.h>
+#include "../include/libtree.h"
+
+//
+// 2分木の初期化
+//
+void initialize(binary_tree* t)
+{
+	t->root = NULL;
+}
+
+//
+// 使用メモリの全解放
+//
+void finalize(binary_tree* t)
+{
+}
+
+//
+// keyの値を見てノードを追加する
+//
+bool add_node(binary_tree* t, int key, const char* value)
+{
+	// todo: ノードを作成して、適切な位置に挿入してください
+
+	return true;
+}
+
+//
+// keyの小さな順にコールバック関数funcを呼び出す
+//
+void serach(const binary_tree* t, void (*func)(const node* p))
+{
+	// todo: keyの小さな順にfunc関数をよびだしてください
+	func(t->root);// テストが通ってしまっていたので、通らなくした
+}
+
